335d63460b0e4bcaf35ca2a2375b7546c34cb5a8
Arun Jojo <arunkjojo@gmail.com>
2022-05-27 12:31:46 +0530

    loadig...
    Signed-off-by: Arun Jojo <arunkjojo@gmail.com>

--- src/App.js
+++ src/App.js
@@ -5,19 +5,29 @@ import ResultData from "./components/Result";
 import SearchIcons from "./components/SearchIcons";
 import { AppDiv, NavIcon, ErrorPage } from "./customStyle";
 import { Icons } from "./DB";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import { updateSuggestion, updatePopularCity } from "./redux/serviceLocation";
 
 var online_error=null;
-if (!navigator.onLine) {
-  online_error = 'Please check the internet connection';
-}
 function App() {
   const dispatch = useDispatch();
+
+  const location = useSelector(state => state.location);
+  if(location.popular.length === 0){
+    console.log(location.popular.length)
+    online_error = 'Loading...'
+  }else{
+    online_error = null;
+  }
+
   useEffect(() => {
     dispatch(updatePopularCity());
     dispatch(updateSuggestion());
-  }, [dispatch]);
+    if(location.popular.length !== 0){
+      online_error = null
+    }
+  }, []);
+
   return (
     online_error!==null 
     ? <ErrorPage><h2>{online_error}</h2></ErrorPage>
